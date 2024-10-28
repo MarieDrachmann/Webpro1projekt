6c404c22475ef31f2a7a243e1b06a4be1fdee41c
MarieDrachmann <144018969+MarieDrachmann@users.noreply.github.com>
2024-10-28 13:29:51 +0100

    Projekt oprettet

---
+++ Webpro1projekt/Webpro1projekt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Webpro1projekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
