8ca449daf2d9fc5edd42d5f47fa7da3fc95b8924
MarieDrachmann <144018969+MarieDrachmann@users.noreply.github.com>
2024-11-06 10:04:20 +0100

    The new project uploaded

---
+++ TheRealProject/WebApplicationTest/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationTest.Models
+{
+    public class User
+    {
+        [RegularExpression("")] //her skriver man RegEx
+        public int id { get; set; }
+
+        [Required] //Gør at man skal skrive det ind
+        public string email { get; set; }
+
+        [Required]
+        public string password { get; set; }
+    }
+}
